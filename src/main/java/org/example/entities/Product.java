4f14d6a7bb5c4f950efecd12e0527b33b492a99a
Jimena Alexandra Ruiz Cerna <jyellow363@gmail.com>
2022-04-20 22:53:59 -0500

    demo commit

---
+++ src/main/java/org/example/entities/Product.java
@@ -0,0 +1,83 @@
+package org.example.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "name", length = 80, nullable = false)
+	private String name;
+	@Column(name = "category", nullable = false)
+	private String category;
+	@Column(name = "unit_price", nullable = false)
+	private double unitPrice;
+	@Column(name = "unit_stock", nullable = false)
+	private double unitStock;
+	
+	@ManyToOne
+	@JoinColumn(name = "supplier_id", nullable = false)
+	private Supplier supplier;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public double getUnitStock() {
+		return unitStock;
+	}
+
+	public void setUnitStock(double unitStock) {
+		this.unitStock = unitStock;
+	}
+
+	public Supplier getSupplier() {
+		return supplier;
+	}
+
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+
+}
